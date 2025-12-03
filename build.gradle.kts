d2ba8dccad5262e887c0c4f34c50d04360cebb5e
kairusds <32636532+kairusds@users.noreply.github.com>
2025-12-03 13:00:04 +0800

    Update Gradle wrapper and Android Gradle Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:8.13.0")
         classpath("com.aliucord:gradle:main-SNAPSHOT") {
             exclude("com.github.js6pak", "jadb")
         }
